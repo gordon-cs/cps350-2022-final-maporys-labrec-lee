dc5da36d0a365e8ee255b81a99374cf07c5b6c79
Charlie Lee <charlie.lee@gordon.edu>
2022-04-23 00:19:24 -0400

    implement working navigation graph and buttons to move to different fragments

--- Maporys/app/src/main/java/com/example/maporys/MainFragment.kt
+++ Maporys/app/src/main/java/com/example/maporys/MainFragment.kt
@@ -8,16 +8,20 @@ import android.widget.Button
 import androidx.annotation.Nullable
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_main.*
 import kotlinx.android.synthetic.main.fragment_main.view.*
 
-class MainFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        val view: View = inflater.inflate(R.layout.fragment_main, container, false)
-        view.reviewEntryButton.setOnClickListener { Navigation.findNavController(view).navigate(R.id.navigateToNewEntry) }
-        return view;
+class MainFragment : Fragment(R.layout.fragment_main) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        newEntryButton.setOnClickListener {
+            val action = MainFragmentDirections.mainFragmentToNewEntryFragment()
+            findNavController().navigate(action)
+        }
+        reviewEntryButton.setOnClickListener {
+            val action = MainFragmentDirections.mainFragmentToReviewEntry()
+            findNavController().navigate(action)
+        }
     }
 }
--- Maporys/app/src/main/java/com/example/maporys/NewEntryFragment.kt
+++ Maporys/app/src/main/java/com/example/maporys/NewEntryFragment.kt
@@ -1,26 +1,22 @@
 package com.example.maporys
 
 import android.os.Bundle
-import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.WindowManager
-import android.widget.Button
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_main.*
+import kotlinx.android.synthetic.main.fragment_new_entry.*
 import kotlinx.android.synthetic.main.fragment_new_entry.view.*
 
-class NewEntryFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_new_entry, container, false)
-        view.cancelButton.setOnClickListener { Navigation.findNavController(view).navigate(R.id.navigateMainFragment) }
-        return view;
+class NewEntryFragment : Fragment(R.layout.fragment_new_entry) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        cancelButton.setOnClickListener {
+            val action = NewEntryFragmentDirections.newEntryFragmentToMainFragment()
+            findNavController().navigate(action)
+        }
     }
-
 }
