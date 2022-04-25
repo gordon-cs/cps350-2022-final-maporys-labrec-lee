acd1d87aee725b8bc55d14ceba7f066110e3ba8b
Charlie Lee <charlie.lee@gordon.edu>
2022-04-25 00:55:20 -0400

    Initialize map placement

--- Maporys/app/src/main/java/com/example/maporys/MainFragment.kt
+++ Maporys/app/src/main/java/com/example/maporys/MainFragment.kt
@@ -4,17 +4,37 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.annotation.Nullable
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.navigation.Navigation
 import androidx.navigation.fragment.findNavController
+import com.example.maporys.databinding.FragmentMainBinding
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_main.*
-import kotlinx.android.synthetic.main.fragment_main.view.*
 
-class MainFragment : Fragment(R.layout.fragment_main) {
+class MainFragment : Fragment(R.layout.fragment_main), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private var mapReady = false
+
+    override fun onCreateView (
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_main, container, false)
+
+        val mapFragment = childFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+        return view
+    }
+
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         newEntryButton.setOnClickListener {
@@ -25,6 +45,14 @@ class MainFragment : Fragment(R.layout.fragment_main) {
             val action = MainFragmentDirections.mainFragmentToReviewEntry()
             findNavController().navigate(action)
         }
+    }
 
+    override fun onMapReady(googleMap : GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val gordon = LatLng(42.59, -70.82)
+        mMap.addMarker(MarkerOptions().position(gordon).title("GoRdOn CoLlEgE"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(gordon, 16F))
     }
 }
