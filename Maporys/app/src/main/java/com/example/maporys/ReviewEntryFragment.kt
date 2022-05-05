540392991867a80bbb22c30efd511ab972752687
Gavin <gavinlabrec@gmail.com>
2022-05-05 00:01:04 -0400

    Add button to see earlier entries at the same location

--- Maporys/app/src/main/java/com/example/maporys/ReviewEntryFragment.kt
+++ Maporys/app/src/main/java/com/example/maporys/ReviewEntryFragment.kt
@@ -2,15 +2,18 @@ package com.example.maporys
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import kotlinx.android.synthetic.main.fragment_review_entry.*
 
 class ReviewEntryFragment : Fragment(R.layout.fragment_review_entry) {
+    private var entryIndex = 0
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        val entry = MainActivity.currentEntries[0]
+        entryIndex = 0
+        val entry = MainActivity.currentEntries[entryIndex]
 
 //        var dateText = findViewById(R.id.dateText) as TextView
         dateText.text = entry.date
@@ -22,5 +25,24 @@ class ReviewEntryFragment : Fragment(R.layout.fragment_review_entry) {
             val action = ReviewEntryFragmentDirections.reviewEntryFragtoMainFrag()
             findNavController().navigate(action)
         }
+
+        olderButton.setOnClickListener {
+            showOlderEntry()
+        }
+
+    }
+
+    // Updates review entry page with information from next oldest entry at that location
+    private fun showOlderEntry() {
+        if (entryIndex < (MainActivity.currentEntries.size - 1)) {
+            val newEntry = MainActivity.currentEntries[++entryIndex]
+            dateText.text = newEntry.date
+            val locationString = (newEntry.lat + ", " + newEntry.lng)
+            locationText.text = (locationString)
+            entryInputMultiLine.text = newEntry.text
+        } else {
+            Toast.makeText(activity, "No older entries", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
